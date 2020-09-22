cdc12565960191ef4a778af0d260f38918c439b6
refkinaldi <refkinaldi26@gmail.com>
2020-09-22 23:48:12 +0700

    merubah inheritance-problem.php, membuat file inheritance.php dan overriding.php

--- inheritance.php
+++ inheritance.php
@@ -6,45 +6,52 @@ class Produk{
          $penerbit,
          $harga,
          $jmlHalaman,
-         $waktuMain,
-         $tipe;
+         $waktuMain;
 
   public function __construct($judul="judul", $penulis="penulis", $penerbit="penerbit", $harga=0,
-  $jmlHalaman=0, $waktuMain=0, $tipe){
+  $jmlHalaman=0, $waktuMain=0){
     $this->judul=$judul;
     $this->penulis=$penulis;
     $this->penerbit=$penerbit;
     $this->harga=$harga;
     $this->jmlHalaman=$jmlHalaman;
     $this->waktuMain=$waktuMain;
-    $this->tipe=$tipe;
   }
 
   public function getLabel(){
     return "$this->judul, $this->penulis, $this->penerbit";
   }
 
-  public function getInfoLengkap(){
+  public function getInfoProduk(){
     $str ="{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
-      if ($this->tipe == "Komik"){
-        $str .= " - {$this->jmlHalaman} Halaman.";
-      } else if ($this->tipe =="Game"){
-        $str .= " - {$this->waktuMain} Jam.";
-      }
     return $str;
   }
 
+}
+
+class Komik extends Produk{
+  public function getInfoProduk(){
+      $str ="Komik : {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) - {$this->jmlHalaman} Halaman.";
+      return $str;
+  }
+}
+
+class Game extends Produk{
+  public function getInfoProduk(){
+      $str ="Game : {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) - {$this->waktuMain} Jam.";
+      return $str;
+  }
 }
 
 class CetakInfoProduk{
   public function cetak( Produk $produk){
     $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
-    return $str;
   }
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
-$produk2 = new Produk("Uncharted", "Neil Druckmen", "Sony Computer", 25000, 0, 50, "Game");
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
+$produk2 = new Game("Uncharted", "Neil Druckmen", "Sony Computer", 25000, 0, 50);
 
 // echo "Komik : ". $produk1->getLabel();
 // echo "<br>";
@@ -53,6 +60,6 @@ $produk2 = new Produk("Uncharted", "Neil Druckmen", "Sony Computer", 25000, 0, 5
 //
 // $informasiProduk = new CetakInfoProduk();
 // echo $informasiProduk->cetak($produk1);
-echo $produk1->getInfoLengkap();
+echo $produk1->getInfoProduk();
 echo "<br>";
-echo $produk2->getInfoLengkap();
+echo $produk2->getInfoProduk();
